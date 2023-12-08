6ec168f100e3c3f7a2c57cff88e1ff6f22ec058e
Nejrup <mikkel@nejrup.dk>
2023-12-08 17:55:34 +0100

    Fix postimage script and dirs

--- board/radxa/cm3io/post-image.sh
+++ board/radxa/cm3io/post-image.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+BOARD_DIR="$(dirname $0)"
 set -e
 
 gzip -fk "${BINARIES_DIR}/Image"
-cp board/radxa/cm3io/cm3io.its "${BINARIES_DIR}"
+cp $BOARD_DIR/cm3io.its "${BINARIES_DIR}"
 (cd "${BINARIES_DIR}" && mkimage -f cm3io.its image.itb)
-support/scripts/genimage.sh -c board/radxa/cm3io/genimage.cfg
+support/scripts/genimage.sh -c $BOARD_DIR/genimage.cfg
